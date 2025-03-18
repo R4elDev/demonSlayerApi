6bfc32cc9b302fd98e517d791e289069fb4bc044
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-03-18 16:29:11 -0300

    Menu Burguer feito junto com a primeira tela estatica

--- burguer.js
+++ burguer.js
@@ -1,1 +1,19 @@
+"use strict"
 
+const imgClick = document.getElementById('imgClick')
+
+
+function abrirMenu(){
+    const menuBurguer = document.getElementById('menuBurguer')
+    if(menuBurguer.style.visibility == "hidden"){
+        menuBurguer.style.visibility = "visible"
+        imgClick.src = "./img/xAoClique.png"
+    }else{
+        menuBurguer.style.visibility = "hidden"
+        imgClick.src = "./img/burguerMenu.svg"
+    }
+}
+
+
+
+imgClick.addEventListener('click',abrirMenu)
