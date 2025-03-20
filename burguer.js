7ccb49ea0423a692337cc3b822a02912105fce5c
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-03-20 12:23:41 -0300

    Terminando O JS do menu e realizado a pagina no HTML do personagem

--- burguer.js
+++ burguer.js
@@ -1,17 +1,48 @@
 "use strict"
 
 const imgClick = document.getElementById('imgClick')
+const ul = document.getElementById('lista_menu')
 
+async function carregar() {
+    const url = `https://api.allorigins.win/get?url=https://www.demonslayer-api.com/api/v1/characters?limit=45`;
+    const response = await fetch(url)
+        
+    const data = await response.json()
 
-function abrirMenu(){
+    return JSON.parse(data.contents).content
+}
+
+async function criarListaDeNomes(){
+    const carregandoDados = await carregar()
+    carregandoDados.forEach(criarLi) 
+}
+
+function criarLi(nome){
+    const button = document.createElement('button')
+    button.classList.add('li_menu_burguer')
+    button.textContent = nome.name
+    console.log(button)
+    ul.appendChild(button)
+    
+}
+
+async function abrirMenu(){
+
     const menuBurguer = document.getElementById('menuBurguer')
+    
     if(menuBurguer.style.visibility == "hidden"){
         menuBurguer.style.visibility = "visible"
         imgClick.src = "./img/xAoClique.png"
+        await criarListaDeNomes()
     }else{
         menuBurguer.style.visibility = "hidden"
         imgClick.src = "./img/burguerMenu.svg"
+        ul.replaceChildren('')
     }
+    
+    
+
+    
 }
 
 
